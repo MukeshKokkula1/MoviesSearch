f988191b1803eecfb64071efdaa971580074b007
MukeshKokkula1 <mukeshkumarkokkula@gmail.com>
2022-02-09 11:40:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Series from "./Series";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  const onChangeHandler = (e) => {
+    setSearch(e.target.value);
+    setData([]);
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    axios
+      .get(
+        `https://gateway.marvel.com:443/v1/public/series?titleStartsWith=${search}&ts=1&apikey=143164d66cb1d7f81f2b92d3d65b65d5&hash=a0913b4e101ae4a9c2ae50478f286a4e`
+      )
+      .then((res) => {
+        setData(res.data.data.results);
+      })
+      .catch((error) => console.log(error));
+    setSearch("");
+  };
+
+  return (
+    <center>
+      <div className="app-container">
+        <h1 className="main-heading">Search Your Favorite Movies</h1>
+        <form onSubmit={onSubmitHandler}>
+          <input
+            type="text"
+            className="form-control input"
+            onChange={onChangeHandler}
+            value={search}
+            placeholder="Enter a Movie Name"
+          />
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+          &nbsp;&nbsp;
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => setData([])}
+          >
+            Clear
+          </button>
+        </form>
+
+        {<div>{data.length > 0 ? <Series data={data} /> : null}</div>}
+      </div>
+    </center>
+  );
+}
+
+export default App;
---
+++ src/Series.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./series.css";
+
+const Series = ({ data }) => {
+  return (
+    <div className="table-contain">
+      <table className="table table-hover ">
+        <thead>
+          <th className="table-heading" scope="col">
+            #
+          </th>
+          <th className="table-heading" scope="col">
+            Name
+          </th>
+          <th className="table-heading" scope="col">
+            Url
+          </th>
+          <th className="table-heading" scope="col">
+            Thumbnail
+          </th>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td className="series-title">{item.title}</td>
+              <td>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                >
+                  {`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                </a>
+              </td>
+              <td>
+                <img
+                  src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                  alt="img"
+                  height="140px"
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Series;
